012baaa3775719eab9548f3d9dafdb17e5f01a2a
Roman Turchyniak <rtur@softserveinc.com>
2016-10-20 09:56:36 +0300

    Final version

---
+++ js/main.js
@@ -0,0 +1,56 @@
+/**
+ * Created by rtur on 18.10.2016.
+ */
+//nav
+ $(document).ready(function() {
+     $( '.dropdown' ).hover(
+         function(){
+             $(this).children('.sub-menu').slideDown(200);
+         },
+         function(){
+             $(this).children('.sub-menu').slideUp(200);
+         }
+     );
+ });
+ //carousel
+(function($) {
+    $(function() {
+        $('.jcarousel').jcarousel();
+
+        $('.jcarousel-control-prev')
+            .on('jcarouselcontrol:active', function() {
+                $(this).removeClass('inactive');
+            })
+            .on('jcarouselcontrol:inactive', function() {
+                $(this).addClass('inactive');
+            })
+            .jcarouselControl({
+                target: '-=1'
+            });
+
+        $('.jcarousel-control-next')
+            .on('jcarouselcontrol:active', function() {
+                $(this).removeClass('inactive');
+            })
+            .on('jcarouselcontrol:inactive', function() {
+                $(this).addClass('inactive');
+            })
+            .jcarouselControl({
+                target: '+=1'
+            });
+
+        $('.jcarousel-pagination')
+            .on('jcarouselpagination:active', 'a', function() {
+                $(this).addClass('active');
+            })
+            .on('jcarouselpagination:inactive', 'a', function() {
+                $(this).removeClass('active');
+            })
+            .jcarouselPagination();
+    });
+})(jQuery);
+
+//select
+$(document).ready(function() {
+    $("select").searchable();
+});
